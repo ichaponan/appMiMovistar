11529e8ec54b4a3c715890157a0c0193c7c976bb
ichaponan <ichaponan@attachmedia.com>
2019-04-22 15:03:08 -0500

    subiendo

--- js/tutorial.js
+++ js/tutorial.js
@@ -1,4 +1,4 @@
-slider = () =>{
+let slider = () =>{
   let dots = '';
   let wrappTuto = document.getElementById('atm-sliderTutorial');
   let slide = wrappTuto.getElementsByClassName('atm-slideTuto');
@@ -12,9 +12,9 @@ slider = () =>{
   for (let i = 0; i < slide.length; i++) {
     slide[i].setAttribute('id', 'slide'+i); /*Añadiendo id*/
     if(i == 0){
-      dots = dots + '<button data-id="'+i+'" class="activeDot"></button>'; /*Añadiendo dots*/
+      dots = dots + '<button id="dots'+i+'" data-id="'+i+'" class="dots activeDot" onclick="slideTo(this)"></button>'; /*Añadiendo dots*/
     } else {
-      dots = dots + '<button data-id="'+i+'"></button>'; /*Añadiendo dots*/
+      dots = dots + '<button id="dots'+i+'" data-id="'+i+'" class="dots" onclick="slideTo(this)"></button>'; /*Añadiendo dots*/
     }
   }
 
@@ -23,9 +23,26 @@ slider = () =>{
   wrapDots.classList.add('atm-dotsSlider');
 
   wrappTuto.appendChild(wrapDots);
+
+
+  /*Añadiendo el dinamismo del slider*/
+  slideTo = (element) => {
+    let id = element.getAttribute('data-id');
+    // console.log(id);
+    removeActive();
+    document.getElementById('slide' + id).classList.add('activeSlide');
+    document.getElementById('dots' + id).classList.add('activeDot');
+  }
+
+  removeActive = () =>{
+    let dots = document.getElementsByClassName('dots');
+    for (let i = 0; i < slide.length; i++) {
+      dots[i].classList.remove('activeDot');
+      slide[i].classList.remove('activeSlide');
+    }
+  }
 }
 
 window.onload = ()=>{
   slider();
-
 };
