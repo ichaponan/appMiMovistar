c533f34d09f8981a400e2a4aba3e7081d3988a14
ichaponan <ichaponan@attachmedia.com>
2019-05-06 11:38:35 -0500

    estructurando js

---
+++ js/scripts.js
@@ -0,0 +1,26 @@
+/**
+  Función general para eliminar un tag en la lista.
+
+  tagElement: elemento a eliminar
+  busqueda: all, 1
+  funcion: funcion de validacion para eliminar el tag
+
+**/ 
+let elementos = {
+  "elementos": [
+    {'tagElement': '.tagelemento', 'busqueda': 'all', 'funcion': funcioncalback()}
+  ]
+}
+
+// let _busquedaTag = () =>{
+//   elementos.map(() =>{
+
+//   });
+// }
+
+
+
+
+_hideTag = (tagElement, busqueda, funcion) =>{
+
+}
