83cc5ad1e15eaddd22ba674a45197a1238fd18df
ichaponan <ichaponan@attachmedia.com>
2019-05-09 10:58:43 -0500

    englobando version

--- js/scripts.js
+++ js/scripts.js
@@ -1,31 +1,31 @@
+let atmAppVersion = function(){
+  let insertAviso = () => {
+    let structureHTML = '<div class="wrappCeluAviso"><section class="atm-section1"><div class="atm-adornos"><img src="http://servicios.movistar.com.pe/images_publica/alertaversion/images/adornos.svg" alt="adornos"></div><div class="atm-celuWrapp"><img src="http://servicios.movistar.com.pe/images_publica/alertaversion/images/celu.svg" alt="celu" class="atm-celu" /><img src="http://servicios.movistar.com.pe/images_publica/alertaversion/images/isologo.svg" alt="isologo" class="atm-isologo"></div><div class="atm-sombra"><img src="http://servicios.movistar.com.pe/images_publica/alertaversion/images/sombra.svg" alt="sombra" /></div></section><section class="atm-section2"><div class="atm-titulo">¡Tenemos una nueva<br />actualización para ti!</div><p>No te quedes atrás</p><button class="atm-updateApp" onclick="openAppStore(this)">Actualizar</button><button class="atm-omitir" onclick="destroyMessage(this)">Omitir</button></section></div>';
 
-let insertAviso = () => {
-  let structureHTML = '<div class="wrappCeluAviso"><section class="atm-section1"><div class="atm-adornos"><img src="http://servicios.movistar.com.pe/images_publica/alertaversion/images/adornos.svg" alt="adornos"></div><div class="atm-celuWrapp"><img src="http://servicios.movistar.com.pe/images_publica/alertaversion/images/celu.svg" alt="celu" class="atm-celu" /><img src="http://servicios.movistar.com.pe/images_publica/alertaversion/images/isologo.svg" alt="isologo" class="atm-isologo"></div><div class="atm-sombra"><img src="http://servicios.movistar.com.pe/images_publica/alertaversion/images/sombra.svg" alt="sombra" /></div></section><section class="atm-section2"><div class="atm-titulo">¡Tenemos una nueva<br />actualización para ti!</div><p>No te quedes atrás</p><button class="atm-updateApp" onclick="openAppStore(this)">Actualizar</button><button class="atm-omitir" onclick="destroyMessage(this)">Omitir</button></section></div>';
+    let wrappApp = document.getElementById('screen-root');
+    let divWrapp = document.createElement("div");
+    divWrapp.setAttribute('id', 'atm-avisoVersion');
+    divWrapp.innerHTML = structureHTML;
+    wrappApp.appendChild(divWrapp);
+  }
 
-  let wrappApp = document.getElementById('screen-root');
-  let divWrapp = document.createElement("div");
-  divWrapp.setAttribute('id', 'atm-avisoVersion');
-  divWrapp.innerHTML = structureHTML;
-  wrappApp.appendChild(divWrapp);
-}
+  let openAppStore = () => {
+    if (navigator.platform != 'iPad' && navigator.platform != 'iPhone' && navigator.platform != 'iPod') {
+      /*Android*/
+      let url = "https://play.google.com/store/apps/details?id=tdp.app.col";
+      "undefined" != typeof deviceapis ? deviceapis.open(url) : open(url);
+    } else {
+      /*IOS*/
+      let url = "https://itunes.apple.com/pe/app/mi-movistar-lite/id1437658799?mt=8";
+      "undefined" != typeof deviceapis ? deviceapis.open(url) : open(url);
+    }
+  }
 
-let openAppStore = () => {
+  let destroyMessage = () => {
+    let wrapp = document.getElementById('atm-avisoVersion').remove();
+  }
+  //alert(navigator.platform);
   if (navigator.platform != 'iPad' && navigator.platform != 'iPhone' && navigator.platform != 'iPod') {
-    /*Android*/
-    let url = "https://play.google.com/store/apps/details?id=tdp.app.col";
-    "undefined" != typeof deviceapis ? deviceapis.open(url) : open(url);
+    insertAviso();
   }
-  else {
-    /*IOS*/
-    let url = "https://itunes.apple.com/pe/app/mi-movistar-lite/id1437658799?mt=8";
-    "undefined" != typeof deviceapis ? deviceapis.open(url) : open(url);
-  }
-}
-
-let destroyMessage = () => {
-  let wrapp = document.getElementById('atm-avisoVersion').remove();
-}
-//alert(navigator.platform);
-if (navigator.platform != 'iPad' && navigator.platform != 'iPhone' && navigator.platform != 'iPod') {
-  insertAviso();
 }
