989d94a5509509595ff39a34950cd62b4e9ee044
ichaponan <ichaponan@attachmedia.com>
2019-05-14 12:12:36 -0500

    last version

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
 alert("version app");
-atmAppVersion = function () {
+var atmAppVersion = function () {
   if (navigator.platform != 'iPad' && navigator.platform != 'iPhone' && navigator.platform != 'iPod') {
-    deleteAviso = function(){
+    devareAviso = function(){
       document.getElementById('atm-avisoVersion').remove();
       // dataLayer.push({
       //   'event': 'atm.event',
@@ -12,7 +12,7 @@ atmAppVersion = function () {
       // });
     }
     updateApp = function () {
-      let url;
+      var url;
       if (navigator.platform != 'iPad' && navigator.platform != 'iPhone' && navigator.platform != 'iPod') {
         /*Android*/
         url = "https://play.google.com/store/apps/details?id=tdp.app.col";
@@ -30,15 +30,12 @@ atmAppVersion = function () {
       // });
     }
 
-    let structureHTML = '<div class="wrappCeluAviso"><section class="atm-section1"><div class="atm-adornos"><img src="http://servicios.movistar.com.pe/images_publica/alertaversion/images/adornos.svg" alt="adornos"></div><div class="atm-celuWrapp"><img src="http://servicios.movistar.com.pe/images_publica/alertaversion/images/celu.svg" alt="celu" class="atm-celu" /><img src="http://servicios.movistar.com.pe/images_publica/alertaversion/images/isologo.svg" alt="isologo" class="atm-isologo"></div><div class="atm-sombra"><img src="http://servicios.movistar.com.pe/images_publica/alertaversion/images/sombra.svg" alt="sombra" /></div></section><section class="atm-section2"><div class="atm-titulo">¡Tenemos una nueva<br />actualización para ti!</div><p>No te quedes atrás</p><button id="atm-updateApp" class="atm-updateApp" onclick="updateApp(this)">Actualizar</button><button id="atm-omitir" class="atm-omitir" onclick="deleteAviso(this)">Omitir</button></section></div>';
+    var structureHTML = '<div class="wrappCeluAviso"><section class="atm-section1"><div class="atm-adornos"><img src="http://servicios.movistar.com.pe/images_publica/alertaversion/images/adornos.svg" alt="adornos"></div><div class="atm-celuWrapp"><img src="http://servicios.movistar.com.pe/images_publica/alertaversion/images/celu.svg" alt="celu" class="atm-celu" /><img src="http://servicios.movistar.com.pe/images_publica/alertaversion/images/isologo.svg" alt="isologo" class="atm-isologo"></div><div class="atm-sombra"><img src="http://servicios.movistar.com.pe/images_publica/alertaversion/images/sombra.svg" alt="sombra" /></div></section><section class="atm-section2"><div class="atm-titulo">¡Tenemos una nueva<br />actualización para ti!</div><p>No te quedes atrás</p><button id="atm-updateApp" class="atm-updateApp" onclick="updateApp(this)">Actualizar</button><button id="atm-omitir" class="atm-omitir" onclick="devareAviso(this)">Omitir</button></section></div>';
 
-    let wrappApp = document.getElementById('screen-root');
-    let divWrapp = document.createElement("div");
+    var wrappApp = document.getElementById('screen-root');
+    var divWrapp = document.createElement("div");
     divWrapp.setAttribute('id', 'atm-avisoVersion');
     divWrapp.innerHTML = structureHTML;
-    let avisoVersion = document.getElementById('atm-avisoVersion');
-    if (avisoVersion == null || avisoVersion == undefined){
-      wrappApp.appendChild(divWrapp);
-    }
+    wrappApp.appendChild(divWrapp);
   }
 }
